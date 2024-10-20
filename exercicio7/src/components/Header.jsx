a96d558afd8e309ca017db96284ad57a62bf401e
Rirfit <lilrirfit@gmail.com>
2024-10-20 11:13:07 -0300

    FC

---
+++ exercicio7/src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <>
+        <h1>Problemas enfrentados por crianças que descobrem de forma tardia a necessidade de usar óculos</h1>
+        <h2>Impactos na Saúde e Desenvolvimento Escolar de Crianças com Diagnóstico Tardio de Problemas Visuais</h2>
+        </>
+    )
+}export default Header
